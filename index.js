a4611f2e573d7ceacd2bc1c1aa3b4554912674c4
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2022-03-14 19:03:31 -0300

    1/x

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const connection = require('./database/database');
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+connection
+.authenticate()
+.then( () => {
+    console.log('Conexão estabelecida com o banco de dados');
+}).catch(error => {
+    console.log(error)
+})
 
 app.get('/', (req,res) => {
     res.render('index');
