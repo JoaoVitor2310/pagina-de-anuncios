ce736e1990fb714ffd49074e4a70d34cae024f8a
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2022-03-16 14:44:37 -0300

    5/x

--- products/ProductsController.js
+++ products/ProductsController.js
@@ -2,15 +2,37 @@ const express = require('express');
 const router = express.Router();
 const slugify = require('slugify');
 const Category = require('../categories/Category');
+const Product = require('./Product');
 
 router.get('/products', (req,res) => {
-    res.send('Rota de produtos');
+    Product.findAll({
+        include: [{model: Category}]
+    }).then( products => {
+        res.render('admin/products/products', {products: products});
+    })
 });
 
 router.get('/admin/products/new', (req,res) => {
     Category.findAll().then(categories => {
         res.render('admin/products/newProduct', {categories: categories});
     })
+});
+
+router.post('/products/save', (req,res) => {
+    let title = req.body.title;
+    let description = req.body.description;
+    let price = req.body.price;
+    let category = req.body.categoy;
+
+    Products.create({
+        title: title,
+        slug: slugify(title),
+        description: description,
+        price: price,
+        categoryId: category
+    }).then( () => {
+        res.redirect('/products')
+    });
 });
 
 module.exports = router;
