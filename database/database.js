fe8d1ebbc9b08c2e667901fed0ecde5ca2a01b92
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2022-03-21 15:20:50 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const connection =  new Sequelize('pagina-anuncios', 'b8031a0f0ba516', 'f7789314', {
     host: 'us-cdbr-east-05.cleardb.net',
-    dialect: 'heroku_41dbae5411a97ad', 
+    dialect: 'mysql', 
     timezone: '-03:00'
 });//mysql://b8031a0f0ba516:f7789314@us-cdbr-east-05.cleardb.net/heroku_41dbae5411a97ad?reconnect=true
 
