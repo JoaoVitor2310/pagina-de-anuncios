46bdb264b20926330cc65ff877c0f3d67d55072d
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-05-02 20:25:34 -0300

    conexão com o banco não é mais na heroku

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const connection =  new Sequelize('heroku_41dbae5411a97ad', 'b8031a0f0ba516', 'f7789314', {
-    host: 'us-cdbr-east-05.cleardb.net',
+const connection =  new Sequelize('pagina-anuncios', 'root', '', {
+    host: 'localhost',
     dialect: 'mysql', 
     timezone: '-03:00'
 });//mysql://b8031a0f0ba516:f7789314@us-cdbr-east-05.cleardb.net/heroku_41dbae5411a97ad?reconnect=true
